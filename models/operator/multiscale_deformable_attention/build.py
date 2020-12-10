9d4c1414f26056f75cc24e97ca7d00839cf5b270
LitingLin <linliting06@live.com>
2020-12-10 20:23:47 +0800

    2020-12-10

--- models/operator/multiscale_deformable_attention/build.py
+++ models/operator/multiscale_deformable_attention/build.py
@@ -14,7 +14,6 @@ def build_extension_cmake(cuda_path=None, verbose=False):
     import sys
     import os
 
-    original_wd = os.getcwd()
     install_path = _current_path
     source_path = os.path.abspath(_current_path)
 
@@ -54,60 +53,57 @@ def build_extension_cmake(cuda_path=None, verbose=False):
         else:
             cmake_parameters.append('-DCMAKE_PREFIX_PATH={}'.format(python_root_path))
 
-    try:
-        shutil.which('cmake')
-        if sys.platform == 'win32':
-            shutil.which('ninja')
+    shutil.which('cmake')
+    if sys.platform == 'win32':
+        shutil.which('ninja')
 
-            if is_anaconda_dist():
-                def apply_envs(envs):
-                    for key, value in envs.items():
-                        os.environ[key] = value
+        if is_anaconda_dist():
+            def apply_envs(envs):
+                for key, value in envs.items():
+                    os.environ[key] = value
 
-                def get_vc_version():
-                    conda_meta_path = os.path.join(sys.prefix, 'conda-meta')
-                    conda_packages = os.listdir(conda_meta_path)
-                    conda_packages = [conda_package for conda_package in conda_packages if
-                                      conda_package.startswith('vc') and conda_package.endswith('.json')]
-                    assert len(conda_packages) == 1
-                    vc_package_path = os.path.join(conda_meta_path, conda_packages[0])
-                    import json
-                    with open(vc_package_path) as fid:
-                        vc_package_meta = json.load(fid)
-                        return vc_package_meta['version']
+            def get_vc_version():
+                conda_meta_path = os.path.join(sys.prefix, 'conda-meta')
+                conda_packages = os.listdir(conda_meta_path)
+                conda_packages = [conda_package for conda_package in conda_packages if
+                                  conda_package.startswith('vc') and conda_package.endswith('.json')]
+                assert len(conda_packages) == 1
+                vc_package_path = os.path.join(conda_meta_path, conda_packages[0])
+                import json
+                with open(vc_package_path) as fid:
+                    vc_package_meta = json.load(fid)
+                    return vc_package_meta['version']
 
-                import distutils._msvccompiler
-                apply_envs(distutils._msvccompiler._get_vc_env('x64 -vcvars_ver={}'.format(get_vc_version())))
+            import distutils._msvccompiler
+            apply_envs(distutils._msvccompiler._get_vc_env('x64 -vcvars_ver={}'.format(get_vc_version())))
 
-        if not os.path.exists(_build_path):
-            os.mkdir(_build_path)
+    if not os.path.exists(_build_path):
+        os.mkdir(_build_path)
 
-        os.chdir(_build_path)
-        import subprocess
-        if sys.platform == 'win32':
-            cmake_command = ['cmake', '-G', 'Ninja', '-DCMAKE_BUILD_TYPE=RelWithDebInfo']
-            cmake_command.extend(cmake_parameters)
-            cmake_command.append(_to_unix_style_path(source_path))
-            subprocess.check_call(cmake_command, cwd=_build_path)
+    os.chdir(_build_path)
+    import subprocess
+    if sys.platform == 'win32':
+        cmake_command = ['cmake', '-G', 'Ninja', '-DCMAKE_BUILD_TYPE=RelWithDebInfo']
+        cmake_command.extend(cmake_parameters)
+        cmake_command.append(_to_unix_style_path(source_path))
+        subprocess.check_call(cmake_command, cwd=_build_path)
 
-            build_command = ['ninja']
-            if verbose:
-                build_command.append('-v')
-            subprocess.check_call(build_command, cwd=_build_path)
-            subprocess.check_call(['ninja', 'install'], cwd=_build_path)
-        else:
-            cmake_command = ['cmake', '-DCMAKE_BUILD_TYPE=RelWithDebInfo']
-            cmake_command.extend(cmake_parameters)
-            cmake_command.append(source_path)
-            subprocess.check_call(cmake_command, cwd=_build_path)
-            import multiprocessing
-            build_command = ['make', '-j{}'.format(multiprocessing.cpu_count())]
-            if verbose:
-                build_command.append('VERBOSE=1')
-            subprocess.check_call(build_command, cwd=_build_path)
-            subprocess.check_call(['make', 'install'], cwd=_build_path)
-    finally:
-        os.chdir(original_wd)
+        build_command = ['ninja']
+        if verbose:
+            build_command.append('-v')
+        subprocess.check_call(build_command, cwd=_build_path)
+        subprocess.check_call(['ninja', 'install'], cwd=_build_path)
+    else:
+        cmake_command = ['cmake', '-DCMAKE_BUILD_TYPE=RelWithDebInfo']
+        cmake_command.extend(cmake_parameters)
+        cmake_command.append(source_path)
+        subprocess.check_call(cmake_command, cwd=_build_path)
+        import multiprocessing
+        build_command = ['make', '-j{}'.format(multiprocessing.cpu_count())]
+        if verbose:
+            build_command.append('VERBOSE=1')
+        subprocess.check_call(build_command, cwd=_build_path)
+        subprocess.check_call(['make', 'install'], cwd=_build_path)
 
 
 def clean_up():
@@ -116,7 +112,6 @@ def clean_up():
         shutil.rmtree(_build_path)
     except:
         pass
-    return
 
 
 if __name__ == '__main__':
