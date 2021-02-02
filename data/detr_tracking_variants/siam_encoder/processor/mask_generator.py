9fd6a31c5805eb7cefafa0419f6ac4a9bcc7dfc6
LitingLin <linliting06@live.com>
2021-02-02 21:33:05 +0800

    2021-2-2

--- data/detr_tracking_variants/siam_encoder/processor/mask_generator.py
+++ data/detr_tracking_variants/siam_encoder/processor/mask_generator.py
@@ -7,9 +7,9 @@ class SiamTransformerMaskGeneratingProcessor:
         self.pre_processor = pre_processor
 
     def __call__(self, image_z: np.ndarray, z_bounding_box: np.ndarray, image_x: np.ndarray, x_bounding_box: np.ndarray, is_positive):
-        z, z_bbox, x, x_bbox = self.pre_processor(image_z, z_bounding_box, image_x, x_bounding_box, is_positive)
+        z, z_bbox, x, x_bbox = self.pre_processor(image_z, z_bounding_box, image_x, x_bounding_box)
 
         c, h, w = z.shape
         z_mask = torch.ones((h, w), dtype=torch.bool)
         z_mask[z_bbox[1]: z_bbox[1] + z_bbox[3], z_bbox[0]: z_bbox[0] + z_bbox[2]] = False
-        return [z, z_mask, x], x_bbox
+        return (z, z_mask, x), x_bbox
