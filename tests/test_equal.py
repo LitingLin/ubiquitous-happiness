7d8d4ecfa66e13aa8bb6f7841812c6e62b3087bd
LitingLin <linliting06@live.com>
2021-02-13 02:02:51 +0800

    2021-2-13

--- tests/test_equal.py
+++ tests/test_equal.py
@@ -3,15 +3,15 @@ import os
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_path)
 
-from Dataset.SOT.Seed.LaSOT import LaSOT_Seed
+from Dataset.SOT.Seed.UAV123 import UAV123_Seed
 from Dataset.Type.data_split import DataSplit
 from Dataset.SOT.factory import SingleObjectTrackingDatasetFactory
-from evaluation.evaluator.got10k.datasets.lasot import LaSOT
+from evaluation.evaluator.got10k.datasets.uav123 import UAV123
 
 
 if __name__ == '__main__':
-    dataset = SingleObjectTrackingDatasetFactory([LaSOT_Seed(data_split=DataSplit.Validation)]).construct()[0]
-    dataset_got = LaSOT(dataset.get_root_path())
+    dataset = SingleObjectTrackingDatasetFactory([UAV123_Seed()]).construct()[0]
+    dataset_got = UAV123(dataset.get_root_path())
     assert len(dataset_got) == len(dataset)
     dataset_name_index_map = {}
     for index_of_sequence, sequence in enumerate(dataset):
