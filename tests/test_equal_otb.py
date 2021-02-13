32961886589513bba83b35c62f7c00cdf86af58e
LitingLin <linliting06@live.com>
2021-02-13 15:54:00 +0800

    2020-2-13

---
+++ tests/test_equal_otb.py
@@ -0,0 +1,32 @@
+import sys
+import os
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(root_path)
+
+from Dataset.SOT.Seed.OTB100 import OTB100_Seed
+from Dataset.Type.data_split import DataSplit
+from Dataset.SOT.factory import SingleObjectTrackingDatasetFactory
+from evaluation.evaluator.got10k.datasets.otb import OTB
+
+from Dataset.Filter.DataCleaning.Integrity import DataCleaning_Integrity
+from Dataset.Filter.DataCleaning.BoundingBox import DataCleaning_BoundingBox
+
+if __name__ == '__main__':
+    dataset = SingleObjectTrackingDatasetFactory([OTB100_Seed()]).construct(filters=[DataCleaning_Integrity()])[0]
+    dataset_got = OTB(dataset.get_root_path())
+    assert len(dataset_got) == len(dataset)
+    dataset_name_index_map = {}
+    for index_of_sequence, sequence in enumerate(dataset):
+        dataset_name_index_map[sequence.get_name()] = index_of_sequence
+    for index in range(len(dataset)):
+        sequence_name = dataset_got.seq_names[index]
+        sequence = dataset[dataset_name_index_map[sequence_name]]
+        _, annos = dataset_got[index]
+        assert len(sequence) == annos.shape[0]
+        bounding_boxes = sequence.get_all_bounding_box()
+        valid_annotation_indices = sequence.get_all_bounding_box_validity_flag()
+        if valid_annotation_indices is not None:
+            bounding_boxes = bounding_boxes[valid_annotation_indices]
+            annos = annos[valid_annotation_indices]
+
+        assert (bounding_boxes == annos).all()
