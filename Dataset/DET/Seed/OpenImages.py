d63b04160bba210e3ac2a388f317c39ae3c444d8
LitingLin <linliting06@live.com>
2021-04-21 22:30:41 +0800

    2021-4-21

--- Dataset/DET/Seed/OpenImages.py
+++ Dataset/DET/Seed/OpenImages.py
@@ -3,7 +3,7 @@ from Dataset.Type.data_split import DataSplit
 
 
 class OpenImages_Seed(BaseSeed):
-    def __init__(self, root_path: str=None, data_split=DataSplit.Full):
+    def __init__(self, root_path: str=None, data_split=DataSplit.Training | DataSplit.Validation | DataSplit.Testing):
         if root_path is None:
             root_path = self.get_path_from_config('Open_Images_PATH')
         super(OpenImages_Seed, self).__init__('Open-Images-V5', root_path, data_split, 2)
