d6b5a014cc0d11036b4aa494b9a31b1d7d638707
LitingLin <linliting06@live.com>
2021-05-14 21:51:47 +0800

    2021-5-14

--- workarounds/all.py
+++ workarounds/all.py
@@ -1,15 +1,15 @@
 def apply_all_workarounds(seed=0):
     from .numpy import numpy_no_multithreading
     # from .opencv import opencv_no_multithreading
-    # from .reproducibility import seed_all_rng, enable_deterministic_computation
-    from .Tensorflow import silence_tensorflow, disable_tensorflow_gpu
-    from .torchvision_tensorflow import fix_torchvision_tensorflow
+    from .reproducibility import seed_all_rng, enable_deterministic_computation
+    # from .Tensorflow import silence_tensorflow, disable_tensorflow_gpu
+    # from .torchvision_tensorflow import fix_torchvision_tensorflow
 
-    fix_torchvision_tensorflow()
+    # fix_torchvision_tensorflow()
     numpy_no_multithreading()
     # opencv_no_multithreading()
-    # seed_all_rng(seed)
+    seed_all_rng(seed)
     # enable_deterministic_computation()
-    silence_tensorflow()
+    # silence_tensorflow()
 
     # disable_tensorflow_gpu()
