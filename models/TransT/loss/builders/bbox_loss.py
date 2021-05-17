34afb4b197f3805bbf625d25b283a653f37ade1d
liting <linliting06@live.com>
2021-05-18 01:16:23 +0800

    2021-5-18

--- models/TransT/loss/builders/bbox_loss.py
+++ models/TransT/loss/builders/bbox_loss.py
@@ -5,6 +5,10 @@ def build_bbox_loss(loss_parameters):
     if 'L1_loss' in loss_parameters['bounding_box_regression']:
         return nn.L1Loss(reduction='sum'), {'loss_iou': loss_parameters['bounding_box_regression']['L1_loss']['weight']}
     elif 'smooth_L1_loss' in loss_parameters['bounding_box_regression']:
-        return nn.SmoothL1Loss(reduction='sum'), {'loss_iou': loss_parameters['bounding_box_regression']['smooth_L1_loss']['weight']}
+        smooth_L1_loss_parameter = loss_parameters['bounding_box_regression']['smooth_L1_loss']
+        beta = 1.0
+        if 'beta' in smooth_L1_loss_parameter:
+            beta = smooth_L1_loss_parameter['beta']
+        return nn.SmoothL1Loss(reduction='sum', beta=beta), {'loss_iou': loss_parameters['bounding_box_regression']['smooth_L1_loss']['weight']}
     else:
         raise RuntimeError('Unknown bounding box loss')
