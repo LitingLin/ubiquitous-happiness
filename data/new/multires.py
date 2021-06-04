463897a5aaa1933b11a0e476e3d8af738c345d6a
liting <linliting06@live.com>
2021-06-04 23:19:54 +0800

    2021-6-4

--- data/new/multires.py
+++ data/new/multires.py
@@ -1,1 +1,13 @@
-def scaling_multi
+import torch
+from data.operator.
+
+
+def scaling_to_multi_resolution(image: torch.Tensor, resolutions):
+    c, h, w = image.shape
+
+    size = w * h
+    scaling = tuple(resolution / size for resolution in resolutions)
+    level = scaling.index(min(scaling))
+
+
+    pass
