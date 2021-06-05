8ed106c12662eeae154faf4b8f111be76b1661fe
LitingLin <linliting06@live.com>
2021-06-05 14:47:06 +0800

    2021-6-4

--- algorithms/tracker/transt/tracker.py
+++ algorithms/tracker/transt/tracker.py
@@ -11,11 +11,13 @@ class TransTTracker(object):
     def initialize(self, image, bbox):
         self.last_frame_object_bbox = bbox
         curated_template_image = self.data_preprocessor.initialize(image, bbox)
+        curated_template_image = curated_template_image.unsqueeze()
         self.z = self.net.template(curated_template_image)
 
     def track(self, image):
         c, h, w = image.shape
         curated_search_image, curation_scaling, curation_source_center_point, curation_target_center_point = self.data_preprocessor.track(image, self.last_frame_object_bbox)
+        curated_search_image = curated_search_image.unsqueeze()
         net_output = self.net.track(self.z, curated_search_image)
         bounding_box = self.label_postprocessor(net_output)
         bounding_box_predicted, self.last_frame_object_bbox = self.bounding_box_postprocessor(bounding_box, (w, h), curation_scaling, curation_source_center_point, curation_target_center_point)
