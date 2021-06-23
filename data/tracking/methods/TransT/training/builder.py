f7ecd3f50a331db406dd805cfd825e0f60f5b37d
liting <linliting06@live.com>
2021-06-24 01:25:52 +0800

    2021-6-24

--- data/tracking/methods/TransT/training/builder.py
+++ data/tracking/methods/TransT/training/builder.py
@@ -1,4 +1,4 @@
-from data.tracking.methods.TransT.training.collate_fn import transt_collate_fn
+from data.tracking.methods.TransT.training.collate_fn import transt_collate_fn, SiamFC_collate_fn
 from data.tracking.methods.TransT.training.processor_stage2 import TransTStage2DataProcessor
 
 
@@ -19,24 +19,24 @@ def _build_transt_data_processor(network_config: dict, train_config: dict, label
                            train_data_augmentation_config['color_jitter'],
                            label_generator,
                            network_data_config['interpolation_mode'],
-                           train_data_augmentation_config['stage_2_on_host_process']), transt_collate_fn
+                           train_data_augmentation_config['stage_2_on_host_process'])
 
 
 def build_transt_data_processor(network_config: dict, train_config: dict):
     if network_config['head']['type'] == 'DETR':
         from .label.transt import TransTLabelGenerator
         label_generator = TransTLabelGenerator(network_config['head']['parameters']['input_size'], network_config['data']['search_size'])
-        return _build_transt_data_processor(network_config, train_config, label_generator)
+        return _build_transt_data_processor(network_config, train_config, label_generator), transt_collate_fn
     elif 'exp-1' in network_config['head']['type']:
         from .label.exp_1 import Exp1LabelGenerator
         label_generator = Exp1LabelGenerator(network_config['head']['parameters']['input_size'], network_config['data']['search_size'], train_config['data']['gaussian_target_label_min_overlap'])
-        return _build_transt_data_processor(network_config, train_config, label_generator)
+        return _build_transt_data_processor(network_config, train_config, label_generator), transt_collate_fn
     elif network_config['head']['type'] == 'Stark':
         pass
     elif network_config['head']['type'] == 'SiamFC':
         from .label.siamfc import SiamFCLabelGenerator
         label_generator = SiamFCLabelGenerator(network_config['head']['input_size'], network_config['head']['r_pos'], network_config['head']['r_neg'], network_config['head']['total_stride'])
-        return _build_transt_data_processor(network_config, train_config, label_generator)
+        return _build_transt_data_processor(network_config, train_config, label_generator), SiamFC_collate_fn
     else:
         raise RuntimeError(f"Unknown {network_config['transformer']['head']['type']}")
 
