be9a6f7080754608bc16d745b4bc1a9fe60fa649
LitingLin <linliting06@live.com>
2021-06-24 10:59:02 +0800

    2021-6-23

--- data/tracking/methods/TransT/training/collate_fn.py
+++ data/tracking/methods/TransT/training/collate_fn.py
@@ -64,7 +64,7 @@ def transt_collate_fn(data):
     else:
         target_bounding_box_label_matrix_batch = None
 
-    is_positives = torch.tensor(is_positives)
+    # is_positives = torch.tensor(is_positives)
 
     return (z_image_batch, x_image_batch), \
            (num_boxes_pos, target_feat_map_indices_batch_id_vector, target_feat_map_indices_batch,
