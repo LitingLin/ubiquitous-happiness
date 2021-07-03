efc0f71600c72915d33a8dbc09ca4ee2c81ea4a0
liting <linliting06@live.com>
2021-07-03 14:00:32 +0800

    2021-7-3

--- models/TransT/module/relative_position_cross_attention.py
+++ models/TransT/module/relative_position_cross_attention.py
@@ -4,33 +4,38 @@ from models.backbone.swint.swin_transformer import _generate_2d_relative_positio
 from timm.models.layers import trunc_normal_
 
 
-def generating_2d_relative_position(x_size, y_size):
-    x_w, x_h = x_size
-    y_w, y_h = y_size
-    two_d_positions = []
-    for i_x_h in range(x_h):
-        for i_x_w in range(x_w):
-            positions = []
-            for i_y_h in range(y_h):
-                y_offset = (i_x_h - i_y_h) * (x_w + y_w - 1)
-                for i_y_w in range(y_w):
-                    positions.append(i_x_w - i_y_w + y_offset)
-            two_d_positions.append(positions)
-
-    two_d_positions = torch.tensor(two_d_positions)
-    two_d_positions -= torch.min(two_d_positions)
-    return two_d_positions
-
-
 class RelativePositionCrossAttention(nn.Module):
     def __init__(self, dim, num_heads, z_2d_shape, x_2d_shape,  # (H, W)
                  qkv_bias=True, qk_scale=None, attn_drop=0.,
                  proj_drop=0.):
         super(RelativePositionCrossAttention, self).__init__()
+        position_bias_table_size = (max(z_2d_shape[0], x_2d_shape[0]), max(z_2d_shape[1], x_2d_shape[1]))
+        relative_position_index_ = _generate_2d_relative_position_index(position_bias_table_size)
 
-        self.register_buffer("relative_position_index", generating_2d_relative_position((z_2d_shape[1], z_2d_shape[0]), (x_2d_shape[1], x_2d_shape[0])))
+        h_z_begin = int(position_bias_table_size[0] / 2 - z_2d_shape[0] / 2)
+        h_x_begin = int(position_bias_table_size[0] / 2 - x_2d_shape[0] / 2)
+        h_z_end = h_z_begin + z_2d_shape[0]
+        h_x_end = h_x_begin + x_2d_shape[0]
+
+        w_z_begin = int(position_bias_table_size[1] / 2 - z_2d_shape[1] / 2)
+        w_x_begin = int(position_bias_table_size[1] / 2 - x_2d_shape[1] / 2)
+        w_z_end = w_z_begin + z_2d_shape[1]
+        w_x_end = w_x_begin + x_2d_shape[1]
+
+        relative_position_index = []
+
+        for h_z in range(h_z_begin, h_z_end):
+            for h_x in range(h_x_begin, h_x_end):
+                for w_z in range(w_z_begin, w_z_end):
+                    z_position = h_z * z_2d_shape[0] + w_z
+                    x_position_begin = h_x * z_2d_shape[1] + w_x_begin
+                    x_position_end = h_x * z_2d_shape[1] + w_x_end
+                    relative_position_index.append(
+                        relative_position_index_[z_position][x_position_begin: x_position_end])
+        relative_position_index = torch.cat(relative_position_index)
+        self.register_buffer("relative_position_index", relative_position_index)
         self.relative_position_bias_table = nn.Parameter(
-            torch.zeros(((z_2d_shape[0] + x_2d_shape[0] - 1) * (z_2d_shape[1] + x_2d_shape[1] - 1), num_heads)))
+            torch.zeros(((2 * position_bias_table_size[0] - 1) * (2 * position_bias_table_size[1] - 1), num_heads)))
 
         self.q_mat = nn.Linear(dim, dim, bias=qkv_bias)
         self.kv_mat = nn.Linear(dim, dim * 2, bias=qkv_bias)
--- models/TransT/variants/relative_position_cross_attention/feature_fusion.py
+++ models/TransT/variants/relative_position_cross_attention/feature_fusion.py
@@ -2,6 +2,36 @@ import torch
 import torch.nn as nn
 from torch.nn import ModuleList
 from .module import CrossAttention, CrossAttentionDecoder
+from .self_attention import SelfAttention
+
+
+class Encoder(nn.Module):
+    def __init__(self, dim, num_heads, z_size, x_size, # (H, W)
+                 drop_path=0.):
+        super(Encoder, self).__init__()
+        self.z_self_attn = SelfAttention(dim, num_heads, z_size, drop_path=drop_path)
+        self.x_self_attn = SelfAttention(dim, num_heads, x_size, drop_path=drop_path)
+        self.cross_attn = CrossAttention(dim, num_heads, z_size, x_size, drop_path=drop_path)
+
+    def forward(self, z, x):
+        z = self.z_self_attn(z)
+        x = self.x_self_attn(x)
+        z, x = self.cross_attn(z, x)
+        return z, x
+
+
+class Decoder(nn.Module):
+    def __init__(self, dim, num_heads, z_size, x_size, # (H, W)
+                 drop_path=0.):
+        super(Decoder, self).__init__()
+        self.z_self_attn = SelfAttention(dim, num_heads, z_size, drop_path=drop_path)
+        self.x_self_attn = SelfAttention(dim, num_heads, x_size, drop_path=drop_path)
+        self.cross_attn = CrossAttentionDecoder(dim, num_heads, z_size, x_size, drop_path=drop_path)
+
+    def forward(self, z, x):
+        z = self.z_self_attn(z)
+        x = self.x_self_attn(x)
+        return self.cross_attn(z, x)
 
 
 class FeatureFusionNetwork(nn.Module):
@@ -9,9 +39,9 @@ class FeatureFusionNetwork(nn.Module):
         super(FeatureFusionNetwork, self).__init__()
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(n_feature_fusion_layers))]  # stochastic depth decay rule
         self.encoder = ModuleList(
-            [CrossAttention(hidden_dim, n_heads, template_size, search_size, drop_path=dpr[i]) for i in range(n_feature_fusion_layers)]
+            [Encoder(hidden_dim, n_heads, template_size, search_size, drop_path=dpr[i]) for i in range(n_feature_fusion_layers)]
         )
-        self.decoder = CrossAttentionDecoder(hidden_dim, n_heads, template_size, search_size)
+        self.decoder = Decoder(hidden_dim, n_heads, template_size, search_size)
 
     def forward(self, z, x):
         z = z.flatten(2).transpose(1, 2)  # N, C, H, W => N, L, C
