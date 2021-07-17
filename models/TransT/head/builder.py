f3c1e26b131b34a4b5523fbdb2c434ffd6381c18
LitingLin <linliting06@live.com>
2021-07-17 18:13:04 +0800

    2021-7-17

--- models/TransT/head/builder.py
+++ models/TransT/head/builder.py
@@ -15,13 +15,16 @@ def build_head(network_config):
             raise RuntimeError(f"Unknown value {head_config['quality_assessment_with']}")
     elif head_config['type'] == 'GFocal-v2':
         head_parameters = head_config['parameters']
-        cls_reg_shared = False
-        if 'shared' in head_parameters:
-            cls_reg_shared = head_parameters['shared']
-        if cls_reg_shared:
-            from .gfocal_shared_mlp import GFocalV2Head
+        if 'pos_encoded' in head_parameters:
+            from .gfocal_pos_encoded_mlp import GFocalV2Head
         else:
-            from .gfocal_v2 import GFocalV2Head
+            cls_reg_shared = False
+            if 'shared' in head_parameters:
+                cls_reg_shared = head_parameters['shared']
+            if cls_reg_shared:
+                from .gfocal_shared_mlp import GFocalV2Head
+            else:
+                from .gfocal_v2 import GFocalV2Head
         return GFocalV2Head(head_parameters['input_dim'], head_parameters['hidden_dim'], head_parameters['input_size'],
                             head_parameters['reg_max'], head_parameters['topk'], head_parameters['reg_channels'],
                             head_parameters['add_mean'])
